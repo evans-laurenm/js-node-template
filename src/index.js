01a80b9ca61d4266a5ffb579f23316e9354e9e92
Lauren Evans <lauren.evans@student.swic.edu>
2025-02-27 22:26:42 -0600

    Add demo code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-var x = 3;
+const y = 7;
+console.log(y);
 
-console.log(x);
+const name = "Lauren Evans";
+console.log("Hello,", name);
+
+const string = "Hello, world!";
+console.log(string);
+
+const person = {
+	name: "Lauren",
+	age: 29,
+	career: "Human Resources",
+	hobbies: ["crocheting", "writing", "hiking"],
+	isVerteran: true,
+	careerGoals: "Data Analyst",
+	favoriteColor: "green",
+};
+console.log(person);
+
+const toDoList = ["Clean House", "Go to Gym", "Do Homework"];
+console.log(toDoList);
